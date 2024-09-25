39d9e18ec00eaead961392726ddf79c80f93fd1a
Matthew French <mattfrench92@gmail.com>
2024-09-24 21:37:21 -0400

    Release process design
    - add API and Client address configurables
    - add release make receipe
    - configure db connection with './db.json'
    - statically serve './static' with deno
    - separate production and development environments for angular

--- api/server.ts
+++ api/server.ts
@@ -1,18 +1,27 @@
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import router from "./router.ts"
 
 const app = new Application();
+
+const client_address = Deno.env.get("ZB_CLIENT")?.toString();
 
 app.use(async (ctx, next) => {
   await next();
   console.log(Date.now()+" - "+ctx.request.method+" - "+ctx.request.headers.get("host")+ctx.request.url.pathname+" -- Res "+ctx.response.status);
 });
 app.use(async (ctx, next) => {
-  ctx.response.headers.set("Access-Control-Allow-Origin", "http://localhost:4200");
+  ctx.response.headers.set("Access-Control-Allow-Origin", client_address || "http://localhost:8080");
   await next();
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.use(async (context) => {
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}/static`,
+    index: "index.html",
+  });
+});
 
 app.listen({ port: 8080 });
 console.log(`Listening on localhost:${8080}`);
